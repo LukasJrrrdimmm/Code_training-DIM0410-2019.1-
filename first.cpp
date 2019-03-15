e29dad64f7b01bd60417b2335b02843893e2916c
Lukas Maximo Grilo Abreu Jardim <lukasmaximo@gmail.com>
2019-03-15 18:33:46 -0300

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int t;
+	scanf("%d", &t);
+	int a[t], b[t];
+	int a_, b_;
+	char r[t];
+	int cnt;
+	for(cnt = 0; cnt < t; cnt++){
+		scanf("%d", &a_);
+		a[cnt] = a_;
+		scanf("%d", &b_);
+		b[cnt] = b_;
+		if(a[cnt] < b[cnt])
+			r[cnt] = '<';
+		else if(a[cnt] == b[cnt])
+			r[cnt] = '=';
+		else
+			r[cnt] = '>';
+	}
+	for(cnt = 0; cnt < t; cnt++){
+		printf("%c\n", r[cnt]);
+	}
+	
+}
